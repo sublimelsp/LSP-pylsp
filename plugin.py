4667da011097ce1563eb75ca0388edcf81fb5965
Raoul Wols <raoulwols@gmail.com>
2020-07-05 12:40:01 +0200

    Update

--- plugin.py
+++ plugin.py
@@ -20,7 +20,7 @@ class PalantirPyls(AbstractPlugin):
     @classmethod
     def additional_variables(cls) -> Optional[Dict[str, str]]:
         variables = {}
-        variables["sublime_py_files_dir"] = os.path.dirname(sublime.__file__)
+        variables['sublime_py_files_dir'] = os.path.dirname(sublime.__file__)
         variables["sublime_packages_dir"] = sublime.packages_path()
         return variables
 
@@ -51,6 +51,11 @@ class PalantirPyls(AbstractPlugin):
         return str(settings.get("pyls_black_version"))
 
     @classmethod
+    def isort_version_str(cls) -> str:
+        settings = sublime.load_settings("LSP-palantir-pyls.sublime-settings")
+        return str(settings.get("pyls_isort_version"))
+
+    @classmethod
     def python_exe(cls) -> str:
         return "python" if sublime.platform() == "windows" else "python3"
 
@@ -62,8 +67,7 @@ class PalantirPyls(AbstractPlugin):
             startupinfo.dwFlags |= flag
         else:
             startupinfo = None
-        return subprocess.check_output(
-            args=args, cwd=kwargs.get("cwd"), startupinfo=startupinfo)
+        return subprocess.check_output(args=args, cwd=kwargs.get("cwd"), startupinfo=startupinfo)
 
     @classmethod
     def needs_update_or_installation(cls) -> bool:
@@ -71,18 +75,24 @@ class PalantirPyls(AbstractPlugin):
             if os.path.exists(cls.pip_exe()):
                 pyls_needs_update = True
                 black_needs_update = True
-                for line in cls.run(
-                        cls.pip_exe(), "list").decode("ascii").splitlines():
+                isort_needs_update = True
+                for line in cls.run(cls.pip_exe(), "list").decode("ascii").splitlines():
                     if line.startswith("python-language-server "):
                         length = len("python-language-server ")
                         stored_version_str = line[length:].strip()
                         if stored_version_str == cls.pyls_version_str():
                             pyls_needs_update = False
                     elif line.startswith("pyls-black "):
-                        stored_version_str = line[len("pyls-black "):].strip()
+                        length = len("pyls-black ")
+                        stored_version_str = line[length:].strip()
                         if stored_version_str == cls.black_version_str():
                             black_needs_update = False
-                return pyls_needs_update or black_needs_update
+                    elif line.startswith("pyls-isort "):
+                        length = len("pyls-isort ")
+                        stored_version_str = line[length:].strip()
+                        if stored_version_str == cls.isort_version_str():
+                            isort_needs_update = False
+                return pyls_needs_update or black_needs_update or isort_needs_update
         return True
 
     @classmethod
@@ -90,13 +100,12 @@ class PalantirPyls(AbstractPlugin):
         shutil.rmtree(cls.basedir(), ignore_errors=True)
         try:
             os.makedirs(cls.basedir(), exist_ok=True)
-            cls.run(cls.python_exe(), "-m", "venv", "LSP-palantir-pyls",
-                    cwd=sublime.cache_path())
-            pyls = "python-language-server[all]=={}".format(
-                cls.pyls_version_str())
+            cls.run(cls.python_exe(), "-m", "venv", "LSP-palantir-pyls", cwd=sublime.cache_path())
+            pyls = "python-language-server[all]=={}".format(cls.pyls_version_str())
             black = "pyls-black=={}".format(cls.black_version_str())
+            isort = "pyls-isort=={}".format(cls.isort_version_str())
             mypy = "git+https://github.com/tomv564/pyls-mypy.git"
-            cls.run(cls.pip_exe(), "install", pyls, black, mypy)
+            cls.run(cls.pip_exe(), "install", pyls, black, mypy, isort)
         except Exception:
             shutil.rmtree(cls.basedir(), ignore_errors=True)
             raise
