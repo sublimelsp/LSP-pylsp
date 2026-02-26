103e508422e5ed5f469256e0b173d9ac375dc599
Rafal Chlodnicki <rchl2k@gmail.com>
2026-02-26 22:55:33 +0100

    feat: manage the server with UV

--- plugin.py
+++ plugin.py
@@ -1,31 +1,53 @@
-from LSP.plugin.core.typing import Dict
-from lsp_utils.pip_client_handler import PipClientHandler
-import os
+from __future__ import annotations
+
+from lsp_utils import GenericClientHandler
+from lsp_utils import UvVenvManager
+from pathlib import Path
+from typing import final
+from typing_extensions import override
 import sublime
 
 
-class Pylsp(PipClientHandler):
-    package_name = __package__
-    requirements_txt_path = "requirements.txt"
-    server_filename = "pylsp"
+@final
+class Pylsp(GenericClientHandler):
+    package_name = str(__package__)
+    uv_venv_manager: UvVenvManager | None = None
 
-    # --- PipClientHandler handlers ------------------------------------------------------------------------------------
-
-    @classmethod
-    def get_python_binary(cls) -> str:
-        settings = sublime.load_settings('{}.sublime-settings'.format(cls.package_name))
-        python_binary = settings.get('python_binary')
-        if python_binary and isinstance(python_binary, str):
-            return python_binary
-        return super().get_python_binary()
+    # --- GenericClientHandler handlers --------------------------------------------------------------------------------
 
     @classmethod
-    def get_additional_variables(cls) -> Dict[str, str]:
+    @override
+    def needs_update_or_installation(cls) -> bool:
+        if not cls.uv_venv_manager:
+            cls.uv_venv_manager = UvVenvManager(cls.package_name, 'server/pyproject.toml', Path(cls.storage_path()))
+        return cls.uv_venv_manager.needs_install_or_update()
+
+    @classmethod
+    @override
+    def install_or_update(cls) -> None:
+        if not cls.uv_venv_manager:
+            raise Exception('Expected UvVenvManager to be initialized')
+        cls.uv_venv_manager.install()
+
+    @classmethod
+    @override
+    def get_additional_variables(cls) -> dict[str, str]:
         variables = super().get_additional_variables()
         variables.update({
-            "sublime_py_files_dir": os.path.dirname(sublime.__file__),
+            'sublime_py_files_dir': str(Path(sublime.__file__).parent),
         })
+        if cls.uv_venv_manager:
+            variables.update({
+                'server_path': str(cls.uv_venv_manager.venv_bin_path / 'pylsp'),
+            })
         return variables
+
+    @classmethod
+    @override
+    def get_additional_paths(cls) -> list[str]:
+        if uv_venv_manager := cls.uv_venv_manager:
+            return [str(uv_venv_manager.venv_bin_path)]
+        return []
 
 
 def plugin_loaded() -> None:
